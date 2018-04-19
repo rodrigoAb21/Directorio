9410c3b773f4ae6a248c9c70385121361a3b3e89
nath <nath.1475369@gmail.com>
2018-04-19 00:28:39 -0400

    nn

--- app/Http/Controllers/RubroController.php
+++ app/Http/Controllers/RubroController.php
@@ -11,4 +11,40 @@ class RubroController extends Controller
         $rubros = Rubro::all();
         return view('Rubros.verRubros', ['rubros' => $rubros]);
     }
+
+    public function index(){
+        $rubros=Rubro::all();
+        return view('Rubros.gestRubros',['rubros'=>$rubros]);
+    }
+
+
+    public function registrar(Request $request){
+        $rubro =new Rubro();
+        $rubro->nombre= $request->nombre;
+        $rubro->icono=$request->icono;
+        $rubro->save();
+
+        return redirect('rubros');
+    }
+
+    public function eliminar($id){
+        $empresas = DB::table('empresa')
+            -> join('rubro','rubro.id','=','empresa.rubro_id')
+            -> where('rubro','=',$id)
+            -> get('empresa.id');
+        $empresas -> ubicaciones() -> delete();
+        $empresas -> delete();
+        $rubro= Rubro::findOrFail($id);
+        $rubro->delete();
+
+        return redirect('gestRubros');
+    }
+
+    public function editar(Request $request, $id){
+        $rubro= Rubro::findOrFail($id);
+        $rubro->nombre=$request->nombre;
+        $rubro->icono=$request->icono;
+        $rubro->update();
+        return redirect('gestRubros');
+    }
 }
